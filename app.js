7ef5f5c68d03f1230fed659e1afb09042d7ba0d0
XujuanChen <xchen022@citymail.cuny.edu>
2022-07-23 18:04:44 -0400

    initial

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+// middleware
+// app.use('/posts', () => {
+//     console.log ("Posts")
+// })
+
+// import router
+const postRoute = require('./routes/posts');
+
+// middleware
+app.use('/posts', postRoute);
+
+// routes
+app.get('/', (req, res) => {
+    res.send("HELLO WORLD")
+})
+
+// connect to DB
+mongoose.connect(process.env.DB_CONNECTION, ()=> console.log("connected to Mongodb"))
+
+
+// listen
+app.listen(3000);
